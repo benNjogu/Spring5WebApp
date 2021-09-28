cec0f8076c976f967b7c7582becaabfb2ee6f1d8
benNjogu <kamandemachafi.gmail.com>
2021-09-28 15:58:00 +0300

    error corrected was using /author instead of /authors when calling

--- spring5webapp/src/main/java/com/keytech/spring5webapp/Controllers/AuthorController.java
+++ spring5webapp/src/main/java/com/keytech/spring5webapp/Controllers/AuthorController.java
@@ -18,7 +18,7 @@ public class AuthorController {
 	}
 
 
-	@RequestMapping("/author")
+	@RequestMapping("/authors")
 	public String getAuthors(Model model) {
 		
 		model.addAttribute("authors", authorRepository.findAll());
